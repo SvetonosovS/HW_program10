2b6aa7d328cd77f882c8828c1ef627d05b3dc641
SvetonosovS <tris23@bk.ru>
2022-10-07 15:53:56 +0300

    prog10

---
+++ Program_10.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int num;
+{
+while (true) 
+    {
+    Console.Write("Введите целое трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        break;
+    Console.WriteLine($"Ошибка ввода");   
+    }  
+Console.Write(num +" -> ");
+if (num <= 99 | num >= 1000)
+    {
+    Console.WriteLine("Ошибка ввода");
+    } 
+else if (num >= 100 && num <= 999)
+    {
+    num = num / 10 % 10;
+    Console.WriteLine(num);
+    } 
+}
